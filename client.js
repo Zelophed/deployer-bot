c43f539fc34ebd6f56229cb661252c8f0744be8a
Zelophed <zefren1@googlemail.com>
2020-07-12 10:37:43 +0200

    remove js files

--- client.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var Discord = require("discord.js");
-exports.client = new Discord.Client({ partials: ['MESSAGE', 'CHANNEL', 'REACTION'] });
-var logger = require("./logger.js");
-exports.client.once('ready', function () {
-    logger.info('ready');
-});
--- util.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-exports.__esModule = true;
-function sentFromValidChannel(message, validChannels) {
-    return validChannels.includes(message.channel.id);
-}
-exports.sentFromValidChannel = sentFromValidChannel;
